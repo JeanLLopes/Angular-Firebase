a574857bc91ba2d56145472adb71b981b87cd5d5
JeanLLopes <jean.llopes@outlook.com>
2016-11-05 18:18:38 -0200

    Envio de Dados para o Firebase

--- index-2.js
+++ index-2.js
@@ -1,3 +1,32 @@
+//firebase URL
+var bancoDados = new Firebase("https://projeto-angularfirebase.firebaseio.com/AngularFirebase");
+
 angular.module('tarefasApp',[]).controller('tarefasCtrl', function($scope){
-    
+
+  //objeto onde serão armazenadas as terefas
+  $scope.tasks = [];
+
+  //VARIAVEIS
+  $scope.tarefa = "";
+
+
+  //FUNÇÕES
+  //ADICIONAR UMA NOVA TAREFA
+  $scope.adicionar = function(){
+      debugger;
+      bancoDados.push({task:$scope.tarefa});
+      $scope.tarefa = "";
+  }
+
+  //DELETAR TAREFA
+  $scope.deletar = function(){
+      $scope.tasks.splice(this.$index,1)
+  }
+
+  //INICIALIZAÇÃO
+  $scope.inicializacao = function(){
+      alert('Iniciando Aplicação')
+  }
+
+
 })
